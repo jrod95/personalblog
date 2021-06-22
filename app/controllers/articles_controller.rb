9d05ca6051cf87fa31648f55ab76b72975fd61d7
Juan Rodriguez <jrcam95@gmail.com>
2021-06-21 21:52:44 -0300

    concerns-applied

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -42,10 +42,9 @@ class ArticlesController < ApplicationController
     redirect_to root_path
   end
 
-
-
   private
-    def article_params
-      params.require(:article).permit(:title, :body)
-    end
+
+  def article_params
+    params.require(:article).permit(:title, :body, :status)
+  end
 end
